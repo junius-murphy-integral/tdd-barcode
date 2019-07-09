d4f268f7dae7f5903eb0110bfe8611903bc24f2a
junius-murphy-integral <junius.murphy@integral.io>
2019-07-09 13:21:31 -0400

    pass all test thus far

--- src/main/java/com/integral/barcode/BarcodeObserver.java
+++ src/main/java/com/integral/barcode/BarcodeObserver.java
@@ -5,11 +5,18 @@ public class BarcodeObserver{
     private String errorMessage = "INVALID BARCODE";
     public void update(String barcode){
         String strippedBarcode = barcode.replace("-", "");
-        this.barcode = strippedBarcode.length() == 13? strippedBarcode : errorMessage;
-        this.barcode = strippedBarcode.matches("[0-9]+")? strippedBarcode: errorMessage;
+        if(strippedBarcode.length() != 13 || !strippedBarcode.matches("[0-9]+"))
+        {
+            this.barcode = errorMessage;
+        }
+        else
+        {
+            this.barcode = strippedBarcode;
+        }
     }
 
-        public String getLastBarcode(){
-        return barcode;
-    }
+        public String getLastBarcode()
+        {
+            return barcode;
+        }
 }
