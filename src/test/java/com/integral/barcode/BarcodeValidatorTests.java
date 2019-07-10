a36fed55c8564260e5346281b4afe58239a387a8
junius-murphy-integral <junius.murphy@integral.io>
2019-07-10 11:42:23 -0400

    can pass standard barcode format

--- src/test/java/com/integral/barcode/BarcodeValidatorTests.java
+++ src/test/java/com/integral/barcode/BarcodeValidatorTests.java
@@ -24,22 +24,26 @@ public class BarcodeValidatorTests {
 
         sale.onBarcode(barcode);
         assertEquals( "$1.11", sale.getDisplay().getLastDisplayedPrice());
-        //assertEquals( BarcodePrices.barcodeByPrices.get(barcode), Display.getLastDisplayedPrice());
 
     }
 
-//    @Test
-//    public void testStandardBarcodeFormatWithRealBarcode()
-//    {
-//        String barcode = "9789332555402";
-//        BarcodeValidator barcodeValidator = new BarcodeValidator();
-//        Sale sale = new Sale();
-//
-//        sale.addObserver(barcodeValidator);
-//        sale.onBarcode(barcode);
-//
-//        assertEquals( BarcodePrices.barcodeByPrices.get(barcode), Display.getLastDisplayedPrice());
-//    }
+    @Test
+    public void testStandardBarcodeFormatWithRealBarcode()
+    {
+        String barcode = "9789332555402";
+        Map<String, String> catalog = new HashMap<String, String>() {{
+            put("1111111111111", "$1.11");
+            put("9789332555402", "$9.99");
+            put("2222222222222", "$2.22");
+            put("3333333333333", "$3.33");
+        }};
+        Display display = new Display();
+        Sale sale = new Sale(display, catalog);
+
+        sale.onBarcode(barcode);
+
+        assertEquals( BarcodePrices.barcodeByPrices.get(barcode), sale.getDisplay().getLastDisplayedPrice());
+    }
 //    @Test
 //    public void testBarcodeWithHyphens()
 //    {
