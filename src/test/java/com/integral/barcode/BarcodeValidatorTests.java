2bae76eaaaa5075d42a1ae1ff0ba6b3207093500
junius-murphy-integral <junius.murphy@integral.io>
2019-07-10 11:47:57 -0400

    change format of test cases

--- src/test/java/com/integral/barcode/BarcodeValidatorTests.java
+++ src/test/java/com/integral/barcode/BarcodeValidatorTests.java
@@ -42,71 +42,97 @@ public class BarcodeValidatorTests {
 
         sale.onBarcode(barcode);
 
-        assertEquals( BarcodePrices.barcodeByPrices.get(barcode), sale.getDisplay().getLastDisplayedPrice());
+        assertEquals("$9.99" , sale.getDisplay().getLastDisplayedPrice());
     }
-//    @Test
-//    public void testBarcodeWithHyphens()
-//    {
-//        String originalBarcode = "978-93-325-5540-2";
-//        String expectedBarcode = "9789332555402";
-//
-//        BarcodeValidator barcodeValidator = new BarcodeValidator();
-//        Sale sale = new Sale();
-//
-//        sale.addObserver(barcodeValidator);
-//        sale.onBarcode(originalBarcode);
-//
-//        assertEquals( BarcodePrices.barcodeByPrices.get(expectedBarcode), Display.getLastDisplayedPrice());
-//    }
-//
-//    @Test
-//    public void testBarcodeWith12Digits()
-//    {
-//
-//        BarcodeValidator barcodeValidator = new BarcodeValidator();
-//        Sale sale = new Sale();
-//
-//        sale.addObserver(barcodeValidator);
-//        sale.onBarcode("978933255540");
-//
-//        assertEquals( "INVALID BARCODE", Display.getLastDisplayedPrice());
-//    }
-//
-//    @Test
-//    public void testBarcodeWithLetters()
-//    {
-//        BarcodeValidator barcodeValidator = new BarcodeValidator();
-//        Sale sale = new Sale();
-//
-//        sale.addObserver(barcodeValidator);
-//        sale.onBarcode("978933255540A");
-//
-//        assertEquals( "INVALID BARCODE", Display.getLastDisplayedPrice());
-//    }
-//
-//
-//    @Test
-//    public void testEmptyBarcode()
-//    {
-//        BarcodeValidator barcodeValidator = new BarcodeValidator();
-//        Sale sale = new Sale();
-//
-//        sale.addObserver(barcodeValidator);
-//        sale.onBarcode("");
-//
-//        assertEquals( "INVALID BARCODE", Display.getLastDisplayedPrice());
-//    }
-//
-//    @Test
-//    public void testBarcodeWithRandomCharacters()
-//    {
-//        BarcodeValidator barcodeValidator = new BarcodeValidator();
-//        Sale sale = new Sale();
-//
-//        sale.addObserver(barcodeValidator);
-//        sale.onBarcode("$&%^*#($*%&-_");
-//
-//        assertEquals( "INVALID BARCODE", Display.getLastDisplayedPrice());
-//    }
-//
+    @Test
+    public void testBarcodeWithHyphens()
+    {
+        String originalBarcode = "978-93-325-5540-2";
+        String expectedBarcode = "9789332555402";
+
+
+        Map<String, String> catalog = new HashMap<String, String>() {{
+            put("1111111111111", "$1.11");
+            put("9789332555402", "$9.99");
+            put("2222222222222", "$2.22");
+            put("3333333333333", "$3.33");
+        }};
+        Display display = new Display();
+        Sale sale = new Sale(display, catalog);
+
+        sale.onBarcode(originalBarcode);
+
+        assertEquals( "$9.99", sale.getDisplay().getLastDisplayedPrice());
+    }
+
+    @Test
+    public void testBarcodeWith12Digits()
+    {
+
+        Map<String, String> catalog = new HashMap<String, String>() {{
+            put("1111111111111", "$1.11");
+            put("9789332555402", "$9.99");
+            put("2222222222222", "$2.22");
+            put("3333333333333", "$3.33");
+        }};
+        Display display = new Display();
+        Sale sale = new Sale(display, catalog);
+
+        sale.onBarcode("978933255540");
+
+        assertEquals( "INVALID BARCODE", sale.getDisplay().getLastDisplayedPrice());
+    }
+
+    @Test
+    public void testBarcodeWithLetters()
+    {
+        Map<String, String> catalog = new HashMap<String, String>() {{
+            put("1111111111111", "$1.11");
+            put("9789332555402", "$9.99");
+            put("2222222222222", "$2.22");
+            put("3333333333333", "$3.33");
+        }};
+        Display display = new Display();
+        Sale sale = new Sale(display, catalog);
+
+        sale.onBarcode("978933255540A");
+
+        assertEquals( "INVALID BARCODE", sale.getDisplay().getLastDisplayedPrice());
+    }
+
+
+    @Test
+    public void testEmptyBarcode()
+    {
+        Map<String, String> catalog = new HashMap<String, String>() {{
+            put("1111111111111", "$1.11");
+            put("9789332555402", "$9.99");
+            put("2222222222222", "$2.22");
+            put("3333333333333", "$3.33");
+        }};
+        Display display = new Display();
+        Sale sale = new Sale(display, catalog);
+
+        sale.onBarcode("");
+
+        assertEquals( "INVALID BARCODE", sale.getDisplay().getLastDisplayedPrice());
+    }
+
+    @Test
+    public void testBarcodeWithRandomCharacters()
+    {
+        Map<String, String> catalog = new HashMap<String, String>() {{
+            put("1111111111111", "$1.11");
+            put("9789332555402", "$9.99");
+            put("2222222222222", "$2.22");
+            put("3333333333333", "$3.33");
+        }};
+        Display display = new Display();
+        Sale sale = new Sale(display, catalog);
+
+        sale.onBarcode("$&%^*#($*%&-_");
+
+        assertEquals( "INVALID BARCODE", sale.getDisplay().getLastDisplayedPrice());
+    }
+
 }
