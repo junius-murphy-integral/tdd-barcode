a5c801502259effe7607c9945810f183ec1258a7
junius-murphy-integral <junius.murphy@integral.io>
2019-07-10 13:22:17 -0400

    rename test file to emphasize the action being tested rather than the class

--- src/test/java/com/integral/barcode/SellOneItemTest.java
+++ src/test/java/com/integral/barcode/SellOneItemTest.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 import static org.junit.Assert.*;
 
-public class BarcodeValidatorTests {
+public class SellOneItemTest {
 
     @Test
     public void testStandardBarcodeFormat()
